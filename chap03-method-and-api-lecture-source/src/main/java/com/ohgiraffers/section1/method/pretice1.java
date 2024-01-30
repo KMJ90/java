688cf93f185da453cee9ddd18facbae41b6c6a35
KMJ90 <kst906388@gmail.com>
2024-01-30 10:45:03 +0900

    java

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section1/method/pretice1.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section1/method/pretice1.java
@@ -3,20 +3,14 @@ package com.ohgiraffers.section1.method;
 public class pretice1 {
     public static void main(String[] args) {
 
-        int num = 8;
-
-        pretice1 pre1 = new pretice1();  //객체생성
-        pre1.method(num);  //사용해주는것
+        int num = 5;
+        pretice1 pre1 = new pretice1();
+        pre1.method(num);
     }
     public void method(int num) {
         String sum = (num % 2 == 0)? "짝수입니다" : "홀수입니다";
         System.out.println("수는" + num + sum);
     }
-
-
-
-
-
 }
 
 
